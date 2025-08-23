066606171a0bf632e117aa2b125158bb0fc8798b
Eric <patinetakiller564@gmail.com>
2025-08-23 12:29:13 -0300

    Se hizo el crud-tasks

--- app.js
+++ app.js
@@ -4,9 +4,6 @@ import dotenv from "dotenv"
 import { routerTask } from "./src/routers/task.routes.js"
 import { routerUser } from "./src/routers/user.routes.js"
 import { startDB } from "./src/config/tasks_users_db.js"
-import { routerLabel } from "./src/routers/label.routes.js"
-import { routerLabelTask } from "./src/routers/label.task.routes.js"
-import { routerProfile } from "./src/routers/profile.routes.js"
 dotenv.config();
 ////////////////////////////////////////////////////////////////
 
@@ -17,10 +14,6 @@ const PORT = process.env.PORT
 app.use(express.json())
 app.use("/api",routerTask)
 app.use("/api",routerUser)
-app.use("/api",routerLabel)
-app.use("/api",routerLabelTask)
-app.use("/api",routerProfile)
-
 
 startDB().then(()=>{
     app.listen(PORT, () => {
